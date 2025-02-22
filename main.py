9199ccc9c76b95aca65be610c60d619d8f9d6f08
Donzin <3439651960@qq.com>
2025-02-22 15:40:34 +0800

    模板

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
-from pkg.plugin.context import register, handler, BasePlugin, APIHost, EventContext
-from pkg.plugin.events import *
-import asyncio
-from pkg.core import core_entities
+from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
+from pkg.plugin.events import *  # 导入事件类
 
-@register(name="AutoReload", 
-         description="定时执行热重载保持连接", 
-         version="1.0", 
-         author="Donzin")
-class AutoReloadPlugin(BasePlugin):
-    
+
+"""
+在收到私聊或群聊消息"hello"时，回复"hello, <发送者id>!"或"hello, everyone!"
+"""
+
+
+# 注册插件
+@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
+class HelloPlugin(BasePlugin):
+
+    # 插件加载时触发
     def __init__(self, host: APIHost):
-        self.host = host
-        self.reload_interval = 1200  # 20分钟
-        self.reload_scopes = [
-            core_entities.LifecycleControlScope.PLATFORM.value
-        ]
-        self._task = None
+        pass
 
+    # 异步初始化
     async def initialize(self):
-        """异步初始化启动定时任务"""
-        self._task = asyncio.create_task(self.schedule_reload())
-        
-    async def schedule_reload(self):
-        """定时重载任务"""
-        while True:
-            try:
-                for scope in self.reload_scopes:
-                    self.host.logger.info(f"执行定时热重载: scope={scope}")
-                    # 调用主程序的重载接口
-                    await self.host.invoke_method(
-                        "lifecycle_control",
-                        "reload",
-                        scope=scope
-                    )
-                await asyncio.sleep(self.reload_interval)
-            except Exception as e:
-                self.host.logger.error(f"热重载任务异常: {str(e)}")
+        pass
 
-    async def unload(self):
-        """插件卸载时取消任务"""
-        if self._task:
-            self._task.cancel()
-            try:
-                await self._task
-            except asyncio.CancelledError:
-                pass
+    # 当收到个人消息时触发
+    @handler(PersonNormalMessageReceived)
+    async def person_normal_message_received(self, ctx: EventContext):
+        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
+        if msg == "hello":  # 如果消息为hello
 
+            # 输出调试信息
+            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+
+            # 回复消息 "hello, <发送者id>!"
+            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
+
+            # 阻止该事件默认行为（向接口获取回复）
+            ctx.prevent_default()
+
+    # 当收到群消息时触发
+    @handler(GroupNormalMessageReceived)
+    async def group_normal_message_received(self, ctx: EventContext):
+        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
+        if msg == "hello":  # 如果消息为hello
+
+            # 输出调试信息
+            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+
+            # 回复消息 "hello, everyone!"
+            ctx.add_return("reply", ["hello, everyone!"])
+
+            # 阻止该事件默认行为（向接口获取回复）
+            ctx.prevent_default()
+
+    # 插件卸载时触发
     def __del__(self):
         pass
